b1262cee6b53348bf3800e40bb2585e06e9571a7
yessense <Akorchemnyj@gmail.com>
2022-12-20 22:35:19 +0300

    Fixed Decoder test

--- tests/model/test_decoder.py
+++ tests/model/test_decoder.py
@@ -7,7 +7,6 @@ class TestDecoder:
     def test_clevr(self):
         # Clevr
         image_size = (3, 128, 128)
-        n_features = 6
         latent_dim = 1024
         batch_size = 2
         decoder = Decoder(image_size=image_size, latent_dim=latent_dim)
@@ -21,7 +20,6 @@ class TestDecoder:
     def test_paired_dsprites(self):
         # Dsprites
         image_size = (1, 64, 64)
-        n_features = 5
         latent_dim = 1024
         batch_size = 2
         decoder = Decoder(image_size=image_size, latent_dim=latent_dim)
